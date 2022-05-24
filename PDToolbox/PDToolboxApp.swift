38d94105dd68e6a9b3241cfd1e8e05db9fe24c1f
DQ Griffin <dquaviusgriffin@gmail.com>
2022-05-24 15:41:38 -0400

    Initial Commit

---
+++ PDToolbox/PDToolboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDToolboxApp.swift
+//  PDToolbox
+//
+//  Created by Dquavius Griffin on 5/24/22.
+//
+
+import SwiftUI
+
+@main
+struct PDToolboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
