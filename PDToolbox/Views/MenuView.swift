424f1cd4e4d8df9edfc3ddd59bb5dd0974861b6b
DQ Griffin <dquaviusgriffin@gmail.com>
2022-05-26 22:12:27 -0400

    Initial commit

---
+++ PDToolbox/Views/MenuView.swift
@@ -0,0 +1,41 @@
+//
+//  MenuView.swift
+//  PDToolbox
+//
+//  Created by Dquavius Griffin on 5/26/22.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    
+    var menu: IVRMenu
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .center) {
+                Text(menu.name)
+                    .font(.title)
+                    .padding(.leading)
+                Text(menu.extensionNumber)
+            }
+            .padding(.vertical)
+            Text("Prompt: \(menu.prompt)")
+                .lineLimit(-1)
+                .padding(.leading)
+            List(menu.actions) { action in
+                KeyPressView(keyPress: action)
+                    .padding()
+                    .border(.white, width: 1)
+            }
+        }
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        let keyPress = IVRKeyPress(key: "1", actionType: "ForwardToExtension", destination: "1128")
+        let menu = IVRMenu(extensionNumber: "2280", name: "Main IVR", prompt: "Thank you for calling", actions: [keyPress])
+        MenuView(menu: menu)
+    }
+}
