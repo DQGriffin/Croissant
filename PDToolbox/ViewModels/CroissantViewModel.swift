a3f80a335e1e79ede5a60baeb2dcdf1f0820bca2
DQ Griffin <dquaviusgriffin@gmail.com>
2022-05-27 11:31:03 -0400

    Add flags for settings and implement sanitizePrompts()

--- PDToolbox/ViewModels/CroissantViewModel.swift
+++ PDToolbox/ViewModels/CroissantViewModel.swift
@@ -15,6 +15,9 @@ class CroissantViewModel: ObservableObject {
     @Published var error = ""
     @Published var hasMenus = false
     @Published var isDone = false
+    @Published var isInsertNewlineEnabled = false
+    @Published var isIsolateExtensionNumberEnabled = false
+    @Published var isSanitizePrompsEnabled = true
     var menus: [IVRMenu] = []
     
     init() {
@@ -48,6 +51,9 @@ class CroissantViewModel: ObservableObject {
     
     func transformCSV(atPath path: URL) {
         readCSV(atPath: path)
+        if isSanitizePrompsEnabled {
+            sanitizePrompts()
+        }
         writeXML()
         Task {
             await MainActor.run {
@@ -56,4 +62,24 @@ class CroissantViewModel: ObservableObject {
             }
         }
     }
+    
+    func sanitizePrompts() {
+        var index = 0
+        
+        while index < menus.count {
+            menus[index].prompt = menus[index].prompt.replacingOccurrences(of: "_", with: "-")
+            menus[index].prompt = menus[index].prompt.replacingOccurrences(of: "*", with: "star")
+            menus[index].prompt = menus[index].prompt.replacingOccurrences(of: "#", with: "pound")
+            menus[index].prompt = menus[index].prompt.replacingOccurrences(of: "@", with: "at")
+            menus[index].prompt = menus[index].prompt.replacingOccurrences(of: "&", with: "and")
+            menus[index].prompt = menus[index].prompt.replacingOccurrences(of: "(", with: "")
+            menus[index].prompt = menus[index].prompt.replacingOccurrences(of: ")", with: "")
+            menus[index].prompt = menus[index].prompt.replacingOccurrences(of: "%", with: "percent")
+            menus[index].prompt = menus[index].prompt.replacingOccurrences(of: "$", with: "")
+            menus[index].prompt = menus[index].prompt.replacingOccurrences(of: "!", with: ".")
+            menus[index].prompt = menus[index].prompt.replacingOccurrences(of: "?", with: ".")
+            index += 1
+        }
+    }
+    
 }
