eb19b55419a1a93b20629825279324c17305c114
DQ Griffin <dquaviusgriffin@gmail.com>
2022-05-28 13:59:01 -0400

    Add AboutView

---
+++ Croissant/Views/AboutView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutView.swift
+//  Croissant
+//
+//  Created by Dquavius Griffin on 5/28/22.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}
