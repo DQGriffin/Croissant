0d2ab18bb7ab3c2af79cfe30fbec1efa63b2eb7b
DQ Griffin <dquaviusgriffin@gmail.com>
2022-05-28 21:53:21 -0400

    Add LibraryAttribution

---
+++ Croissant/Models/LibraryAttribution.swift
@@ -0,0 +1,14 @@
+//
+//  LibraryAttribution.swift
+//  Croissant
+//
+//  Created by Dquavius Griffin on 5/28/22.
+//
+
+import Foundation
+
+struct LibraryAttribution: Identifiable {
+    let id = UUID()
+    let name: String
+    let link: String
+}
