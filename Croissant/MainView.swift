4771247538b928eb17c794b872861f2eec8dbcf8
DQ Griffin <dquaviusgriffin@gmail.com>
2022-06-01 09:46:45 -0400

    Add toasts

--- Croissant/MainView.swift
+++ Croissant/MainView.swift
@@ -6,69 +6,68 @@
 //
 
 import SwiftUI
+import AlertToast
 
 struct MainView: View {
     
     @ObservedObject var viewModel = IVRToolsViewModel()
     @State var isShowingAudit = false
+    @State var isShowingToast = false
     
     var body: some View {
-        if !isShowingAudit {
-            VStack {
-                Spacer()
-                Image(systemName: "arrow.down.doc")
-                    .resizable()
-                    .frame(width: 75, height: 95)
-                Text("Drag and drop BRD here")
-                    .font(.largeTitle)
-                    .padding()
-                VStack(alignment: .leading) {
-                    Text("1. Drag and drop the BRD into this window")
-                        .font(.caption2)
-                    Text("2. Check your downloads folder for the generated Auto-Receptionist.xml file")
-                        .font(.caption2)
-                    Text("3. Upload the file in Service Web")
-                        .font(.caption2)
-                }
+        VStack {
+            Spacer()
+            Image(systemName: "arrow.down.doc")
+                .resizable()
+                .frame(width: 75, height: 95)
+            Text("Drag and drop BRD here")
+                .font(.largeTitle)
                 .padding()
-                Text(viewModel.status)
-                    .padding(.bottom)
-                if viewModel.isDone {
-                    Button {
-                        isShowingAudit = true
-                    } label: {
-                        Text("View Audit")
-                    }
-
-                }
-                Spacer()
+            VStack(alignment: .leading) {
+                Text("1. Drag and drop the BRD into this window")
+                    .font(.caption2)
+                Text("2. Check your downloads folder for the generated Auto-Receptionist.xml file")
+                    .font(.caption2)
+                Text("3. Upload the file in Service Web")
+                    .font(.caption2)
             }
-            .frame(width: 450, height: 450)
-            .onDrop(of: ["public.url", "public.file-url"], isTargeted: nil) { providers in
-                if let item = providers.first {
-                    if let identifier = item.registeredTypeIdentifiers.first {
-                        if identifier == "public.ur" || identifier == "public.file-url" {
-                            item.loadItem(forTypeIdentifier: identifier, options: nil) { urlData, error in
-                                if let urlData = urlData as? Data {
-                                    let url = NSURL(absoluteURLWithDataRepresentation: urlData, relativeTo: nil) as URL
-                                    viewModel.transform(atPath: url)
-                                }
+            .padding()
+            if viewModel.isDone {
+                Button {
+                    //isShowingAudit = true
+                    isShowingToast = true
+                } label: {
+                    Text("View Audit")
+                }
+                
+            }
+            Spacer()
+        }
+        .frame(width: 450, height: 450)
+        .onDrop(of: ["public.url", "public.file-url"], isTargeted: nil) { providers in
+            if let item = providers.first {
+                if let identifier = item.registeredTypeIdentifiers.first {
+                    if identifier == "public.ur" || identifier == "public.file-url" {
+                        item.loadItem(forTypeIdentifier: identifier, options: nil) { urlData, error in
+                            if let urlData = urlData as? Data {
+                                let url = NSURL(absoluteURLWithDataRepresentation: urlData, relativeTo: nil) as URL
+                                viewModel.transform(atPath: url)
                             }
                         }
                     }
                 }
-                return true
             }
+            return true
         }
-        else {
-            List(viewModel.menus) { menu in
-                MenuView(menu: menu)
-                    .padding(.bottom)
-                Divider()
-            }
-            //.frame(width: 500, height: 450)
-        }
-        
+        .toast(isPresenting: $viewModel.hasMessage, duration: 5, tapToDismiss: false, offsetY: 15, alert: {
+            AlertToast(displayMode: .hud, type: .complete(.green), title: viewModel.message)
+        }, onTap: {
+            print("Toast clicked")
+        }, completion: {
+            //isShowingToast = false
+            viewModel.hasMessage = false
+            print("Toast finished")
+        })
     }
 }
 
