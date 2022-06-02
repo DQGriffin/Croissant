ebf85cca137d0fe88786f364178fb5f4614d6f0a
DQ Griffin <dquaviusgriffin@gmail.com>
2022-06-01 20:28:57 -0400

    Add AlertToast attribution

--- Croissant/ViewModels/AboutViewModel.swift
+++ Croissant/ViewModels/AboutViewModel.swift
@@ -19,6 +19,7 @@ class AboutViewModel: ObservableObject {
         attributions.append(LibraryAttribution(name: "CoreXLSX", link: "https://github.com/CoreOffice/CoreXLSX"))
         attributions.append(LibraryAttribution(name: "XMLCoder", link: "https://github.com/CoreOffice/XMLCoder"))
         attributions.append(LibraryAttribution(name: "ZIPFoundation", link: "https://github.com/weichsel/ZIPFoundation"))
+        attributions.append(LibraryAttribution(name: "AlertToast", link: "https://github.com/elai950/AlertToast"))
     }
     
 }
