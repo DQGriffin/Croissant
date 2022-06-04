2c672b6ca9eb4902bd5046c3db67c92e17c999bb
DQ Griffin <dquaviusgriffin@gmail.com>
2022-06-03 23:29:37 -0400

    Add RenderDefaults

---
+++ Croissant/Call Flow Rendering/RenderDefaults.swift
@@ -0,0 +1,15 @@
+//
+//  RenderDefaults.swift
+//  Croissant
+//
+//  Created by Dquavius Griffin on 6/3/22.
+//
+
+import Foundation
+
+struct RenderDefaults {
+    static var width = 100
+    static var height = 50
+    static var horizontalPadding = 150
+    static var verticalPadding = 150
+}
