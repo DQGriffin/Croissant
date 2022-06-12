719ec6b02063e987f4d7083cb92e8ca70bb96102
DQ Griffin <dquaviusgriffin@gmail.com>
2022-06-12 18:20:51 -0400

    Add GraphObject

---
+++ Croissant/Call Flow Graph/GraphObject.swift
@@ -0,0 +1,22 @@
+//
+//  GraphObject.swift
+//  Croissant
+//
+//  Created by Dquavius Griffin on 6/12/22.
+//
+
+import Foundation
+
+protocol GraphObject {
+    var name: String { get }
+    var extensionNumber: String { get }
+    var phoneNumbers: [String]? { get }
+    var type: GraphObjectType { get }
+}
+
+enum GraphObjectType: Int, Codable {
+    case user
+    case limitedExtension
+    case callQueue
+    case ivrMenu
+}
