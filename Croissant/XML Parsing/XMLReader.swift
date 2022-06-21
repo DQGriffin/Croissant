002f4dcaaabdf345611d01dba0f2f94b3b472f9a
DQ Griffin <dquaviusgriffin@gmail.com>
2022-06-20 20:08:28 -0400

    Give audit file the same name as the BRD

--- Croissant/XML Parsing/XMLReader.swift
+++ Croissant/XML Parsing/XMLReader.swift
@@ -11,13 +11,15 @@ import XMLCoder
 struct XMLReader {
     var ivr: MultiLevelIVR
     
-    init(path: String) {
+    init(path: URL) {
         do {
-            let xmlData = try String(contentsOfFile: path)
+            let xmlData = try String(contentsOfFile: path.path)
             ivr = try! XMLDecoder().decode(MultiLevelIVR.self, from: Data(xmlData.utf8))
             
+            let filename = path.lastPathComponent.replacingOccurrences(of: ".xml", with: ".csv")
+            
             let auditWriter = AuditWriter(multiLevelIVR: ivr)
-            auditWriter.writeIVR()
+            auditWriter.writeIVR(filename: filename)
             
         }
         catch {
